d946373d8e70aba4ad9f2ef355d06d3f590f9e97
HARSHWORKBOOK\Harsh Shah <shahharsh903@gmail.com>
2017-10-16 16:27:17 +0530

    added passport module and google stratigy

---
+++ server/config/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+googleClientID: '6880795370-ca68u2shtsi1mtbkfqf476fdgb07de99.apps.googleusercontent.com',
+googleClientSecret: 'spXgbXPMfmg9yGD7gAlEUh2z'
+};
--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
-app.get('/',(req,res) =>
-{
-	res.send({hi:'harsh'});
-});
-app.listen(5000);
+passport.use(new GoogleStrategy());
+
+// app.get('/',(req,res) =>
+// {
+// 	res.send({hi:'harsh'});
+// });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
 
 
-// http://localhost:5000/
+
+// http://localhost:5000
+
