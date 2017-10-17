d2ceb5db673c808a6d4b1a3db1902a9205ed46a6
HARSHWORKBOOK\Harsh Shah <shahharsh903@gmail.com>
2017-10-17 12:01:29 +0530

    added mongoos and db connection

--- server/config/key.js
+++ server/config/key.js
@@ -1,4 +1,5 @@
 module.exports = {
 googleClientID: '6880795370-ca68u2shtsi1mtbkfqf476fdgb07de99.apps.googleusercontent.com',
-googleClientSecret: 'spXgbXPMfmg9yGD7gAlEUh2z'
+googleClientSecret: 'spXgbXPMfmg9yGD7gAlEUh2z',
+mongoURI:'mongodb://127.0.0.1:27017/emaily-dev'
 };
--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const  mongoose = require('mongoose');
 require ('./services/passport'); 
+const keys = ('./config/key.js');
 const authRoutes = require('./routes/authRoutes');
-const app = express();
 
-// require ('./services/passport')(app);
+
+
+const app = express(keys.mongoURI);
+
+
 authRoutes(app);
 
 
@@ -11,5 +16,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
 // http://localhost:5000
-
-
+// mongoose.connect('mongodb://127.0.0.1:27017/emaily-dev');
+// require ('./services/passport')(app);
